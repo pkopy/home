3fe140fdbbfdeeb2c4b2c22d55fba3016295d71c
pkopy <pawel.kopycki@gmail.com>
2020-07-14 14:16:14 +0200

    adding new component Rekuper.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,14 @@ import './App.css';
 import blue from './images/blue1.gif'
 import green from './images/green1.gif'
 import went from './images/went.svg'
+import Rekuper from "./Rekuper/Rekuper";
 
 function App() {
     return (
-        <div style={{position: "relative"}}>
+        <>
 
-            <div style={{position: "absolute"}}>
-                <img src={blue}/>
-            </div>
-            <div style={{position: "absolute"}}>
-                <img src={green}/>
-            </div>
-            <div className="went" >
-                <img src={went} width={32} style={{marginLeft:'auto', marginRight:'auto'}}/>
-            </div>
-        </div>
+            <Rekuper/>
+        </>
 
     );
 }
---
+++ src/Rekuper/Rekuper.js
@@ -0,0 +1,28 @@
+
+import React from "react";
+import blue from "../images/blue1.gif";
+import green from "../images/green1.gif";
+import went from "../images/went.svg";
+
+export default function Rekuper(props) {
+    return (
+        <div style={{position: "relative", backgroundColor:"#bdbdbd", height:300, width: 300, border: "2px solid", display: 'flex', justifyContent:"center"}}>
+
+                <div style={{position: "absolute"}}>
+                    <img src={blue}/>
+
+                </div>
+                <div style={{position: "absolute"}}>
+                    <img src={green}/>
+                </div>
+                <div className="went">
+                    <img src={went} width={32} style={{marginLeft: 'auto', marginRight: 'auto'}}/>
+                </div>
+                <div className="went1">
+                    <img src={went} width={32} style={{marginLeft: 'auto', marginRight: 'auto'}}/>
+                </div>
+
+
+        </div>
+    )
+}
